e041c3df6e6ae5d3e45595cf318cab7606c022b7
Andres Moron <andresmoron622@gmail.com>
2019-05-25 09:52:57 -0400

    Creando carpetas MVVM

---
+++ Andres Moron Parcial2/Andres Moron Parcial2/Andres Moron Parcial2/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Andres_Moron_Parcial2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
