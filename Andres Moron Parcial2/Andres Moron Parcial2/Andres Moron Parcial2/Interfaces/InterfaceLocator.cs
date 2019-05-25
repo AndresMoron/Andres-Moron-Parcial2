156406776889d26680587d91851d2072f0c903a7
Andres Moron <andresmoron622@gmail.com>
2019-05-25 12:05:46 -0400

    MVVM con carpeta

---
+++ Andres Moron Parcial2/Andres Moron Parcial2/Andres Moron Parcial2/Interfaces/InterfaceLocator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Andres Moron Parcial2.Interfaces
+{
+    interface InterfaceLocator
+    {
+    }
+}
---
+++ Andres Moron Parcial2/Andres Moron Parcial2/Andres Moron Parcial2/Model/Notas.cs
@@ -0,0 +1,22 @@
+﻿namespace App.Model
+
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Notas
+    {
+        [Key]
+        public int NotesId { get; set; }
+        public string Contents { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public Notas()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ModifiedDate = DateTime.Now;
+        }
+
+    }
+}
